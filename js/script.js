9adb30b7abb2c3e1e05e734747320c41a8e807d1
MrDominguezTeco <dominguezedwin1817@gmail.com>
2019-11-07 23:25:34 +0000

    I did the javascript based on replit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-$( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
-
-
-  
-  
-  
-  
+$("button").click(function(){
+    let text = $("input").val();
+    console.log(text);
+    let words = text.split(" ");
+    console.log("words",words);
+    let beg1 = words[0].charAt(0);
+    let beg2 = words[1].charAt(0);
+    console.log(beg1);
+    console.log(beg2);
+    let end1 = words[0].slice(1, words[0].length);
+    let end2 = words[1].slice(1, words[1].length); 
+    console.log("end2",end2);
+    console.log(beg2+end1+beg1+end2);
+    $("#result").append(beg2+end1+ " ",beg1+end2);
 });
